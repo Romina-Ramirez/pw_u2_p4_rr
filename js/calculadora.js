08943ca805bbd0d1d014a0d94e1caf01396002da
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-14 19:48:07 -0500

    Taller 16

---
+++ js/calculadora.js
@@ -0,0 +1,33 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            resultMostrado: true,
+            pantalla: ''
+        }
+    },
+
+    methods: {
+        mostrar(elemento) {
+            if (this.resultMostrado == true) {
+                this.pantalla = elemento;
+                this.resultMostrado = false
+            } else {
+                this.pantalla += elemento;
+            }
+        },
+
+        calcular() {
+            try {
+                resultado = eval(this.pantalla);
+                if (isNaN(resultado)) {
+                    throw new Error("Invalid expression");
+                }
+                this.pantalla = resultado;
+            } catch (error) {
+                this.pantalla = "ERROR";
+            }
+        }
+    }
+})
+
+app.mount('#miApp4')
--- js/vFor.js
+++ js/vFor.js
@@ -17,32 +17,33 @@ const app = Vue.createApp({
     data() {
         return {
             miArreglo: arreglo,
-            nombre: ''
+            nombre: undefined,
+            apellido: undefined
         }
     },
 
     methods: {
-        agregarEstudiante(evento) {
-            console.log('Vamos a agregar')
-            console.log(this.nombre)
-            console.log(evento)
-            console.log(evento.charCode)
-            if (evento.charCode == 13) {
-                console.log('Presiono Enter')
-                const nuevoEstudiante = {
-                    nombre: this.nombre,
-                    apellido: 'Nuevo apellido'
-                }
-                this.miArreglo.unshift(nuevoEstudiante)
-            } else {
-                console.log('No es  Enter')
-            }
-        },
+        // agregarEstudiante(evento) {
+        //     console.log('Vamos a agregar')
+        //     console.log(this.nombre)
+        //     console.log(evento)
+        //     console.log(evento.charCode)
+        //     if (evento.charCode == 13) {
+        //         console.log('Presiono Enter')
+        //         const nuevoEstudiante = {
+        //             nombre: this.nombre,
+        //             apellido: 'Nuevo apellido'
+        //         }
+        //         this.miArreglo.unshift(nuevoEstudiante)
+        //     } else {
+        //         console.log('No es  Enter')
+        //     }
+        // },
         agregarEstudiante2() {
             console.log('Vamos a agregar')
             const nuevoEstudiante = {
                 nombre: this.nombre,
-                apellido: 'Nuevo apellido'
+                apellido: this.apellido
             }
             this.miArreglo.unshift(nuevoEstudiante)
         }
